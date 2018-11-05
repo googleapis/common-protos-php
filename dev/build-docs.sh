0bc3c17350a37423da29391fa3d0e6082dd896b5
michaelbausor <michaelbausor@google.com>
2018-11-05 12:09:01 -0800

    Add travis badge to readme, update travis build (#1)
    * Add travis badge to readme
    * Fix path to dev scripts
    * Remove VERSION file checks, pass master arg

--- dev/build-docs.sh
+++ dev/build-docs.sh
@@ -26,7 +26,6 @@ set -ev
 ROOT_DIR=$(pwd)
 DOC_OUTPUT_DIR=${ROOT_DIR}/tmp_gh-pages
 INDEX_FILE=${DOC_OUTPUT_DIR}/index.html
-VERSION_FILE=${ROOT_DIR}/VERSION
 SAMI_EXECUTABLE=${ROOT_DIR}/vendor/sami/sami/sami.php
 SAMI_CONFIG=${ROOT_DIR}/dev/sami-config.php
 
@@ -37,31 +36,18 @@ UPDATED_INDEX_FILE=$(cat << EndOfMessage
 EndOfMessage
 )
 
-function checkVersionFile() {
-  # Verify that the specified version matches the contents
-  # of the VERSION file.
-  VERSION_FILE_CONTENTS="$(cat ${VERSION_FILE})"
-  if [ ${VERSION_FILE_CONTENTS} != ${1} ]; then
-    echo ERROR: mismatched versions
-    echo Expected version: ${1}
-    echo VERSION file: ${VERSION_FILE_CONTENTS}
-    exit 1
-  fi
-}
-
 function buildDocs() {
   DOCS_VERSION_TO_BUILD=${1}
   COMMON_PROTOS_DOCS_VERSION=${DOCS_VERSION_TO_BUILD} php ${SAMI_EXECUTABLE} update ${SAMI_CONFIG} -v
 }
 
 if [[ ! -z ${TRAVIS_TAG} ]]; then
-  checkVersionFile ${TRAVIS_TAG}
   buildDocs ${TRAVIS_TAG}
   # Update the redirect index file only for builds that use the
   # TRAVIS_TAG env variable.
   echo ${UPDATED_INDEX_FILE} > ${INDEX_FILE}
 elif [[ ! -z ${1} ]]; then
-    buildDocs ${1}
+  buildDocs ${1}
 else
   echo ERROR: TRAVIS_TAG not found, and no command line version specified
   exit 1
