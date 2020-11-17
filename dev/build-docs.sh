15aca62f4035be57c1317980660d7d0b8c88ba8b
Brent Shaffer <betterbrent@google.com>
2020-11-17 07:32:09 -0800

    chore: move to github actions (#15)

--- dev/build-docs.sh
+++ dev/build-docs.sh
@@ -14,41 +14,34 @@
 # limitations under the License.
 
 # Script to build doc site.
-# This script expects to be invoked from the common-protos-php root.
-#
-# This script will look for the TRAVIS_TAG environment variable, and
-# use that as the version number. If no environment variable is found,
-# it will use the first command line argument. If no command line
-# argument is specified, default to 'master'.
+# This script expects to be invoked from the common-protos-php root. It requires
+# a git tag as the first argument.
 
 set -ev
 
+if [[ $# -lt 1 ]]
+then
+    echo "Usage: $0 <tag>"
+    exit 1
+fi
+
+GIT_TAG_NAME=$1
+
 ROOT_DIR=$(pwd)
 DOC_OUTPUT_DIR=${ROOT_DIR}/tmp_gh-pages
-INDEX_FILE=${DOC_OUTPUT_DIR}/index.html
-SAMI_EXECUTABLE=${ROOT_DIR}/vendor/sami/sami/sami.php
-SAMI_CONFIG=${ROOT_DIR}/dev/sami-config.php
-
-# Construct the base index file that redirects to the latest version
-# of the docs. This will only be generated when TRAVIS_TAG is set.
-UPDATED_INDEX_FILE=$(cat << EndOfMessage
-<html><head><script>window.location.replace('/common-protos-php/${TRAVIS_TAG}/' + location.hash.substring(1))</script></head><body></body></html>
-EndOfMessage
-)
 
 function buildDocs() {
-  DOCS_VERSION_TO_BUILD=${1}
-  COMMON_PROTOS_DOCS_VERSION=${DOCS_VERSION_TO_BUILD} php ${SAMI_EXECUTABLE} update ${SAMI_CONFIG} -v
+  SAMI_EXECUTABLE=${ROOT_DIR}/vendor/sami/sami/sami.php
+  SAMI_CONFIG=${ROOT_DIR}/dev/sami-config.php
+  COMMON_PROTOS_DOCS_VERSION=${GIT_TAG_NAME} php ${SAMI_EXECUTABLE} update ${SAMI_CONFIG} -v
 }
 
-if [[ ! -z ${TRAVIS_TAG} ]]; then
-  buildDocs ${TRAVIS_TAG}
-  # Update the redirect index file only for builds that use the
-  # TRAVIS_TAG env variable.
-  echo ${UPDATED_INDEX_FILE} > ${INDEX_FILE}
-elif [[ ! -z ${1} ]]; then
-  buildDocs ${1}
-else
-  echo ERROR: TRAVIS_TAG not found, and no command line version specified
-  exit 1
-fi
+buildDocs ${GIT_TAG_NAME}
+
+# Construct the base index file to redirects to the latest version of the docs.
+UPDATED_INDEX_FILE=$(cat << EndOfMessage
+<html><head><script>window.location.replace('/common-protos-php/${GIT_TAG_NAME}/' + location.hash.substring(1))</script></head><body></body></html>
+EndOfMessage
+)
+echo ${UPDATED_INDEX_FILE} > ${DOC_OUTPUT_DIR}/index.html
+
